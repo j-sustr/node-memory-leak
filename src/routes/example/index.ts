12f05046a0023989642749c44351cab2afdba1b1
Jan Šustr <jan.sustr11@gmail.com>
2025-08-09 12:40:21 +0200

    init

---
+++ src/routes/example/index.ts
@@ -0,0 +1,13 @@
+import { FastifyPluginAsync } from 'fastify'
+
+const leakyData: any[] = [];
+
+const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.get('/', async function (request, reply) {
+    // Add a large object to the array on each request
+    leakyData.push(new Array(1000000).join('x'));
+    return `this is an example. Leaky data size: ${leakyData.length}`
+  })
+}
+
+export default example
